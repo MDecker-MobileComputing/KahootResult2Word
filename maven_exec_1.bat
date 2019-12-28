cf75ab9be8058239725df5bb2d1b6b6c9f8dc552
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-12-28 23:27:52 +0100

    Adjusted maven_exec_1+2.sh|bat, exported two Eclipse Run Configs, unit test method for cmdline args --topline.

--- maven_exec_1.bat
+++ maven_exec_1.bat
@@ -1,4 +1,4 @@
 
 
 @REM Execute goal "java" of plugin "exec", see also https://www.mojohaus.org/exec-maven-plugin/java-mojo.html
-mvn exec:java -Dexec.mainClass=de.mide.kahoot.result2word.Main -Dexec.args="ExampleFiles/input_result_1.xlsx"
+mvn exec:java -Dexec.mainClass=de.mide.kahoot.result2word.Main -Dexec.args="-infile ExampleFiles/input_result_1.xlsx"
--- src/test/java/de/mide/kahoot/test/CmdLineArgsParserTests.java
+++ src/test/java/de/mide/kahoot/test/CmdLineArgsParserTests.java
@@ -1,6 +1,9 @@
 package de.mide.kahoot.test;
 
 import static org.junit.Assert.fail;
+
+import java.util.Optional;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
@@ -70,6 +73,41 @@ public class CmdLineArgsParserTests {
 		assertFalse( cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE) );
 
 		assertEquals(INPUT_FILE, cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_I_FOR_INPUT_FOLDER)); 		
+	}
+	
+	
+	/**
+	 * Test for method {@code de.mide.kahoot.result2word.utils.CmdLineArgsParser.getToplineText()}.
+	 * 
+	 * @throws ParseException  Test failed
+	 */
+	@Test
+	public void topline() throws ParseException {
+		
+		final String INPUT_FOLDER = "path/to/input/folder";		
+		
+		
+		String[] args = new String[]{"-f", INPUT_FOLDER};
+		CommandLine cmdLine = CmdLineArgsParser.parseCommandLineArguments(args);
+		
+		assertTrue(  cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE) );
+		
+		Optional<String> optionalString = CmdLineArgsParser.getToplineText();
+		
+		assertFalse( optionalString.isPresent() );
+		
+		
+		// now the same, but with topline text
+		final String TOPLINE_TEXT = "Lorem Ipsum";
+		
+		args = new String[]{"-f", INPUT_FOLDER, "-t", TOPLINE_TEXT};
+		cmdLine = CmdLineArgsParser.parseCommandLineArguments(args);
+		
+		assertTrue( cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE) );
+		
+		optionalString = CmdLineArgsParser.getToplineText();
+		assertTrue( optionalString.isPresent() );
+		assertEquals( TOPLINE_TEXT, optionalString.get()); 
 	}
 	
 }
