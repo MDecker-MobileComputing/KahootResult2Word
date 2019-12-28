341be2173d6e2465a5212799b86b3f114da10b96
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-12-28 10:38:28 +0100

    When program execution is aborted then blank line is written after error message in console.

--- src/main/java/de/mide/kahoot/result2word/Main.java
+++ src/main/java/de/mide/kahoot/result2word/Main.java
@@ -35,223 +35,223 @@ import de.mide.kahoot.result2word.utils.CmdLineArgsParser;
  * This project is licensed under the terms of the GNU GENERAL PUBLIC LICENSE version 3 (GPL v3).
  */
 public class Main {
-	
-	/** Result code (RC) for aborting program with {@code System::exit(int)} when inconsistent command line arguments are specified. */ 
+
+	/** Result code (RC) for aborting program with {@code System::exit(int)} when inconsistent command line arguments are specified. */
 	protected static final int RESULT_CODE_ON_INVALID_ARGS = 1;
-	
-	/** Result code (RC) for aborting program with {@code System::exit(int)} when exception during reading of input file or writing of docx files has occured. */ 
+
+	/** Result code (RC) for aborting program with {@code System::exit(int)} when exception during reading of input file or writing of docx files has occured. */
 	protected static final int RESULT_CODE_ON_EXCEPTION_DURING_PROCESSING = 2;
-	
+
 	/** Result code (RC) for aborting program with {@code System::exit(int)} when target folder specified with {@code -outfolder} does not exist. */
 	protected static final int RESULT_CODE_TARGET_FOLDER_NOT_EXISTING = 3;
 
-	
+
 	/** Output folder that can be optionally specified by user with cmdline option {@code --outfolder}. */
 	protected static Optional<String> sOutputFolderOptional = Optional.empty();
-	
-	
+
+
     /**
      * Entry point of the program execution.<br><br>
      *
      * @param args  Command line arguments, to be analyzed by {@link CmdLineArgsParser#parseCommandLineArguments(String[])}.
      */
     public static void main(String[] args)  {
-        
-        CommandLine cmdLine = null;        
-        
+
+        CommandLine cmdLine = null;
+
         try {
-            
-            cmdLine = parseCommandLineArguments( args );                        
+
+            cmdLine = parseCommandLineArguments( args );
         }
         catch (ParseException ex) {
-            
+
             printHelpOnCmdLineArgs();
             System.exit( RESULT_CODE_ON_INVALID_ARGS );
         }
-        
-        
-        abortBasedOnCommandLineArgsIfNeeded( cmdLine ); 
-        
+
+
+        abortBasedOnCommandLineArgsIfNeeded( cmdLine );
+
         loadLanguage( cmdLine );
-        
+
         if (cmdLine.hasOption(CMDLINE_OPTION_LETTER_O_FOR_OUTPUT_FOLDER)) {
-        	
+
         	String outputFolder   = cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_O_FOR_OUTPUT_FOLDER);
         	sOutputFolderOptional = Optional.of(outputFolder);
         }
 
-        
+
         // When we come to this line, then programm was started either with cmdline option -i <inputFolder> or -f <inputFile>
-        
+
         try {
-                        
+
             if (cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE)) {
-                                                                
+
             	proccessSingleExcelFile( cmdLine );
-            	
+
             } else {
-            	
+
             	processAllExcelFilesInDirectory( cmdLine );
             }
-        } 
+        }
         catch (KahootException ex) {
-            
+
             System.err.println( "Error: " + ex.getMessage() );
             ex.printStackTrace();
-            
+
             System.exit( RESULT_CODE_ON_EXCEPTION_DURING_PROCESSING );
         }
     }
-    
-    
+
+
     /**
-     * Method for processing when program was called to process single xlsx file 
+     * Method for processing when program was called to process single xlsx file
      * (which will be read from {@code CmdLineArgsParser}).
-     * 
+     *
      * @param cmdLine  Object with result of parsing command line arguments.
-     * 
+     *
      * @throws KahootException  Something went wrong during performing "xlsx2docx".
      */
     protected static void proccessSingleExcelFile(CommandLine cmdLine) throws KahootException {
-    	
+
     	String inputFileName = cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE);
-    	
+
     	xlsx2docx( inputFileName );
     }
-    
+
 
     /**
-     * Method for processing when program was called to process all xlsx files form a particular folder 
+     * Method for processing when program was called to process all xlsx files form a particular folder
      * (which will be read from {@code CmdLineArgsParser}).
-     * 
+     *
      * @param cmdLine  Object with result of parsing command line arguments.
-     * 
+     *
      * @throws KahootException  Something went wrong during performing "xlsx2docx" or when no xlsx files are found
      *                          in the specified folder.
      */
     protected static void processAllExcelFilesInDirectory(CommandLine cmdLine) throws KahootException {
-    	
+
     	String inputFolder= cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_I_FOR_INPUT_FOLDER);
-    	
+
     	String[] xlsxFilesInFolderStringArray = findAllXlsxFilesInDirectory( inputFolder );
-    	
+
     	int numOfXlsxFiles = xlsxFilesInFolderStringArray.length;
     	if (numOfXlsxFiles == 0) {
-    		
+
     		throw new KahootException("No xlsx files found in folder \"" + inputFolder + "\".");
     	}
-    	
+
     	System.out.println("\nNumber of xlsx files found in input folder: " + numOfXlsxFiles + "\n");
-    	
+
     	for (String fileName: xlsxFilesInFolderStringArray) {
-    		
+
     		xlsx2docx( fileName );
-    		
-    		System.out.println("  file  \"" + fileName + "\" was processed.");    		    		
+
+    		System.out.println("  file  \"" + fileName + "\" was processed.");
     	}
-    	
+
     	System.out.println();
     }
-    
-    
+
+
     /**
      * Programmatic consistency checks of command line arguments:
-     * When neither option {@code -i} nor {@code -f} is specified, then program executed is aborted with result 
+     * When neither option {@code -i} nor {@code -f} is specified, then program executed is aborted with result
      * code {@link #RESULT_CODE_ON_INVALID_ARGS}; the program is also aborted when both options are specified
      * at the same time (they are not compatible().
-     * 
+     *
      * @param cmdLine  Object with result of parsing command line arguments.
-     */    
+     */
     protected static void abortBasedOnCommandLineArgsIfNeeded(CommandLine cmdLine) {
-    	
+
         if ( cmdLine.hasOption(CMDLINE_OPTION_LETTER_H_FOR_HELP) ) {
-            
+
             printHelpOnCmdLineArgs();
             System.exit(0);
-        }    	
-    	
-        
+        }
+
+
         if (!cmdLine.hasOption(CMDLINE_OPTION_LETTER_I_FOR_INPUT_FOLDER) && !cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE)) {
-            
-            System.out.println("\nNeither Command Line Option -i nor -f was specified, aborting program.");
+
+            System.out.println("\nNeither Command Line Option -i nor -f was specified, aborting program.\n");
             System.exit(RESULT_CODE_ON_INVALID_ARGS);
         }
-                
+
         if (cmdLine.hasOption(CMDLINE_OPTION_LETTER_I_FOR_INPUT_FOLDER) && cmdLine.hasOption(CMDLINE_OPTION_LETTER_F_FOR_INPUT_FILE)) {
 
-            System.out.println("\nBoth command line options -i and -f were specified, aborting program.");
-            System.exit(RESULT_CODE_ON_INVALID_ARGS);        	
+            System.out.println("\nBoth command line options -i and -f were specified, aborting program.\n");
+            System.exit(RESULT_CODE_ON_INVALID_ARGS);
         }
-        
+
         if (cmdLine.hasOption(CMDLINE_OPTION_LETTER_O_FOR_OUTPUT_FOLDER)) {
-        	
+
         	String  targetFolder       = cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_O_FOR_OUTPUT_FOLDER);
         	boolean targetFolderExists = checkIfDirectoryExists(targetFolder);
-        	
+
         	if (targetFolderExists == false) {
-        		System.out.println("\nTarget folder \"" + targetFolder + "\" does not exist or is not a folder, aborting program.");
+        		System.out.println("\nTarget folder \"" + targetFolder + "\" does not exist or is not a folder, aborting program.\n");
         		System.exit(RESULT_CODE_TARGET_FOLDER_NOT_EXISTING);
         	}
         }
     }
-    
-    
+
+
     /**
      * Evaluate command line argument to set locale.
-     * 
+     *
      * @param cmdLine  Object with result of parsing command line arguments.
      */
     protected static void loadLanguage(CommandLine cmdLine) {
-    	
+
         if (cmdLine.hasOption(CMDLINE_OPTION_LETTER_L_FOR_LOCALE)) {
-            
+
             String localeCode = cmdLine.getOptionValue(CMDLINE_OPTION_LETTER_L_FOR_LOCALE);
             Locale locale     = new Locale(localeCode);
 
             TranslatedTextsProvider.loadResourceBundle(locale);
-            
+
             writeWarningWhenLocaleIsNotSupported(locale);
-            
+
         } else {
-        
+
             TranslatedTextsProvider.loadResourceBundle(Locale.ENGLISH);
-        }    	
+        }
     }
-    
-    
+
+
     /**
-     * Perform the actual work: read one input file {@code pathToInputExcel} and write one word file. 
-     * 
+     * Perform the actual work: read one input file {@code pathToInputExcel} and write one word file.
+     *
      * @param pathToInputExcel  Relative path to Excel file with Kahoot results to be read; suffix {@code .xlsx}
      *                          is replaced with {@code .docx} to obtain name of target file.
-     * 
+     *
      * @throws KahootException  Something went wrong
      */
     protected static void xlsx2docx(String pathToInputExcel) throws KahootException {
-    
+
         QuestionList questionList = null;
-        
-        
+
+
         KahootResultXlsxReader xlsxReader = new KahootResultXlsxReader(pathToInputExcel);
-        
+
         // read input file (Excel file with results downloaded from Kahoot)
         questionList = xlsxReader.extractQuestionList();
 
-        
+
         System.out.println( "\n" + questionList.toString() + "\n");
-        
-        
+
+
         String pathToOutputWord = StringUtils.changeFilenameExtensionXlsx2Docx(pathToInputExcel);
-        
+
         if (sOutputFolderOptional.isPresent()) {
-        	
+
         	pathToOutputWord = changeOutputFolder(pathToOutputWord, sOutputFolderOptional.get());
         }
-        
+
         KahootResultDocxWriter docxWriter = new KahootResultDocxWriter(questionList, pathToOutputWord);
-        
+
         docxWriter.writeResultFile();
-        
+
         System.out.println("Target file written: " + pathToOutputWord);
     }
 
